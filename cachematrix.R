3cea67902c3b283de75ddb9e764388d66e621831
sl-stat <stat.shuo.li@gmail.com>
2015-02-20 12:03:18 -0500

    updated cachematrix.R, ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I wrote a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {                      # set the value of the matrix
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x                       # get the value of the matrix
+    setsolve <- function(solve) m <<- solve   # set the value of the inverse. Function "solve" computes the inverse of a square matrix 
+    getsolve <- function() m                  # get the value of the inverse
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")       # if inverse is not null, return the inverse
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)                    # otherwise, compute the inverse
+    x$setsolve(m)
+    m                                        # Return a matrix that is the inverse of 'x'
 }
---
+++ testingcode.R
@@ -0,0 +1,22 @@
+# Test your code
+source("draft.R")
+#
+# generate matrix, and the inverse of the matrix.
+size <- 100 # size of the matrix edge, don't make this too big
+mymatrix <- matrix(rnorm(size^2), nrow=size, ncol=size)
+mymatrix.inverse <- solve(mymatrix)
+#
+# now solve the matrix via the cache-method
+#
+special.matrix   <- makeCacheMatrix(mymatrix)
+#
+# this should take long, since it's the first go
+special.solved.1 <- cacheSolve(special.matrix)
+#
+# this should be lightning fast
+special.solved.2 <- cacheSolve(special.matrix)
+#
+# check if all solved matrices are identical
+identical(mymatrix.inverse, special.solved.1) & identical(mymatrix.inverse, special.solved.2)
+#
+# should return TRUE
